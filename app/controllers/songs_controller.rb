98e091b748adafd51cd807900d3eef7c2c04a3c7
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-16 18:36:59 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,13 +4,14 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
-  get '/songs/:slug' do
-    erb :'songs/show'
-  end
-
   get '/songs/new' do
 
     erb :'songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
   end
 
   post '/songs' do
