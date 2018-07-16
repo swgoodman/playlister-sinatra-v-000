02f36a551f6bcf5dcd85ac1182b540c202140906
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-16 18:10:47 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,26 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+
+  end
+
+  get '/songs/:slug' do
+
+  end
+
+  get '/songs/new' do
+
+  end
+
+  post '/songs' do
+
+  end
+
+  get '/songs/:slug/edit' do
+
+  end
+
+  patch '/songs/:slug' do
+
+  end
+end
